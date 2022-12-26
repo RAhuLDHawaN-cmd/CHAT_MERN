c97761d2620675e2f66bd735cebb5046d5a98939
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2022-12-26 10:44:37 -0500

    addded live update

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,50 +1,53 @@
-const User= require("../Models/UserModel");
-const bcrypt= require("bcrypt");
-
-module.exports.register = async(req,res,next)=>{
-    console.log(req.body)
-
-    try {
-        const { username, email, password } = req.body;
-        const usernameCheck = await User.findOne({ username });
-        if (usernameCheck)
-          return res.json({ msg: "Username already used", status: false });
-        const emailCheck = await User.findOne({ email });
-        if (emailCheck)
-          return res.json({ msg: "Email already used", status: false });
-        const hashedPassword = await bcrypt.hash(password, 10); // 10 salt level
-
-        const user = await User.create({
-          email,
-          username,
-          password: hashedPassword,
-        });
-        user.save();
-        delete user.password;
-
-        console.log("user added")
-        return res.json({ status: true, user });
-      } catch (ex) {
-        next(ex);
-      }
-    
-}
-
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 module.exports.login = async (req, res, next) => {
   try {
     const { username, password } = req.body;
-    console.log(req.body);
     const user = await User.findOne({ username });
     if (!user)
       return res.json({ msg: "Incorrect Username or Password", status: false });
     const isPasswordValid = await bcrypt.compare(password, user.password);
-
     if (!isPasswordValid)
       return res.json({ msg: "Incorrect Username or Password", status: false });
     delete user.password;
-    
     return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
   } catch (ex) {
     next(ex);
   }
@@ -55,7 +58,7 @@ module.exports.setAvatar = async (req, res, next) => {
     const userId = req.params.id;
     const avatarImage = req.body.image;
     const userData = await User.findByIdAndUpdate(
-       userId,
+      userId,
       {
         isAvatarImageSet: true,
         avatarImage,
@@ -71,15 +74,11 @@ module.exports.setAvatar = async (req, res, next) => {
   }
 };
 
-module.exports.getAllUsers = async (req, res, next) => {
+module.exports.logOut = (req, res, next) => {
   try {
-    const users = await User.find({ _id: { $ne: req.params.id } }).select([
-      "email",
-      "username",
-      "avatarImage",
-      "_id",
-    ]);
-    return res.json(users);
+    if (!req.params.id) return res.json({ msg: "User id is required " });
+    onlineUsers.delete(req.params.id);
+    return res.status(200).send();
   } catch (ex) {
     next(ex);
   }
--- server/index.js
+++ server/index.js
@@ -1,31 +1,51 @@
-const mongoose=require("mongoose");
-
-const express=require('express');
-
-const app=express();
-const UserRoutes=require("./routes/UserRoutes")
-const messagesRoute= require("./routes/messagesRoute.js")
-
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
-const cors=require("cors")
 app.use(cors());
-
 app.use(express.json());
 
-app.use("/api/auth", UserRoutes)
-app.use("/api/messages", messagesRoute)
+mongoose
+  .connect("mongodb+srv://rahul4:test123@cluster0.ar70d.mongodb.net/ChatDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected to Chat");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-mongoose.connect("mongodb+srv://rahul4:test123@cluster0.ar70d.mongodb.net/ChatDB",{useNewUrlParser:true,
-useUnifiedTopology:true}).then(()=>{
-    console.log("DB Connected to Chat")
-}).catch((err)=>{
-    console.log("Error while connecting"+ err.message)
-})
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
 
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
-app.listen(5000, (req,res)=>{
-    console.log("server is hot at "+ process.env.PORT)
-})
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
