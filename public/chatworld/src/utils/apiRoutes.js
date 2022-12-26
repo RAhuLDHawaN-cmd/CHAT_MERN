f6412167665e4aa8bdd38f0c34a9360bfe379bbc
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2022-12-26 11:12:31 -0500

    old files deleted

--- public/chatworld/src/utils/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-const host="http://localhost:5000";
-
-export const regRoute=`${host}/api/auth/register`;
-
-export const loginRoute=`${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;
-
-export const allUsersRoute = `${host}/api/auth/allusers`;
