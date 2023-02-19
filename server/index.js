401de598f499a231a449c8ac7b057109424d474e
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-02-19 10:27:12 -0500

    update port to global

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
+const PORT=process.env.PORT || 6010
 require("dotenv").config();
 
 app.use(cors());
@@ -25,8 +26,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+const server = app.listen(PORT, () =>
+  console.log(`Server started on ${PORT}`)
 );
 const io = socket(server, {
   cors: {
